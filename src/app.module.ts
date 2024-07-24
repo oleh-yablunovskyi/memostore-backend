acd5fd300b8db842df573511a22caae2d440c9a2
Oleh Yablunovskyi <lifserv@gmail.com>
2024-07-24 14:33:53 +0300

    Add SSL configs for DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,18 @@ import { AppService } from './app.service';
 import { QuestionsModule } from './questions/questions.module';
 import { CategoriesModule } from './categories/categories.module';
 
+function getSSLConfig(env: string) {
+  const configs = {
+    production: { require: true, rejectUnauthorized: true },
+    deploy: { require: true, rejectUnauthorized: false },
+    local: false,
+  };
+  if (!configs[env] === undefined) {
+    throw new Error('Set network in your .env file');
+  }
+  return configs[env];
+}
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -22,6 +34,7 @@ import { CategoriesModule } from './categories/categories.module';
         database: configService.get<string>('DATABASE_NAME'),
         logging: ["error"],
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        ssl: getSSLConfig(process.env.SERVER_MODE),
         synchronize: true, // note: set to false in production!
       }),
       inject: [ConfigService],
